ae1118cfaa065aef37760c76a63f5ab4ac3ebe0e
Nithisx <nithishkumarnk182005@gmail.com>
2025-06-13 13:12:42 +0530

    fix css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import AppRoutes from "./AppRoutes";
 import Loader from "./Pages/Loder";
+import "./App.css";
 export default function App() {
   return (
     <div className="App">
